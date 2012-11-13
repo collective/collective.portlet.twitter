6c4741a621ab6a465e2b2c25b95a1cd9d3888d75
Cleber J Santos <cleber@cleberjsantos.com.br>
2012-11-13 14:48:25 -0200

    Fix package

--- src/collective/twitter/widget/__init__.py
+++ src/collective/twitter/widget/__init__.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from zope.i18nmessageid import MessageFactory
+from zope.i18nmessageid import MessageFactory as BaseMessageFactory
 
-_ = MessageFactory('collective.twiiter.widget')
-
-
-# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
-
-
+_ = BaseMessageFactory('collective.twiiter.widget')
---
+++ src/collective/twitter/widget/interfaces.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+from zope.interface import Interface
+
+
+class ITwitterWidgetLayer(Interface):
+    """
+    Marker Default browser layer this product.
+    """
+
